03da48cc465fb3b8c8b512ee1b15de8e553afc3c
yaseswi.n23 <yaseswi.n23@gmail.com>
2024-07-02 12:22:06 +0530

    pushNow

--- src/RRDcomponents/NewProjects.jsx
+++ src/RRDcomponents/NewProjects.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import {
+  Row,
+  Col,
+  CardBody,
+  Card,
+  Alert,
+  Container,
+  ButtonGroup,
+  Button
+} from 'reactstrap';
 
 export default function NewProjects() {
   return (
-    <div>
+    <React.Fragment>
       <h1>New Projects</h1>
-    </div>
+      <Container>
+      <Row className="my-4">
+        <Col>
+          <Alert color="primary">
+            This is a primary alert!
+          </Alert>
+        </Col>
+      </Row>
+      <Row>
+        <Col md="6">
+          <Card>
+            <CardBody>
+              This is the content of the card.
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="6">
+          <ButtonGroup>
+            <Button color="primary">Left</Button>
+            <Button color="secondary">Middle</Button>
+            <Button color="primary">Right</Button>
+          </ButtonGroup>
+        </Col>
+      </Row>
+    </Container>
+    </React.Fragment>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import { UserContextProvider } from './context/UserContext';
 import { UsersDataProvider } from './context/Users';
 import { BrowserRouter } from 'react-router-dom';
+// src/index.js or src/App.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
