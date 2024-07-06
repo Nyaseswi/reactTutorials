f8531097b31b5287ef8e66ba2d3ac8ea9b10d55a
yaseswi.n23 <yaseswi.n23@gmail.com>
2024-07-06 12:33:24 +0530

    class

---
+++ src/components/HelloReact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class HelloReact extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            // text: 'Hello React'
+            text: ''
+        }
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <input type='text' onChange={e => this.setState({ text: e.target.value })} />
+                <h1>{this.state.text.split('').reverse().join('')}</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default HelloReact
